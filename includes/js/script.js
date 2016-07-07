22166ba2db1baf82ae819e3b40da6d6d1b20ef5d
Edsel <edsel@mesasix.com>
2016-07-07 22:29:08 +0800

    addtl section, fixes

--- includes/js/script.js
+++ includes/js/script.js
@@ -11,21 +11,25 @@ Notes:	 Hand coded for Udemy.com
 
 $(function() {
 	
-	$('#alertMe').click(function(e) {
+	// $('#alertMe').click(function(e) {
 		
-		e.preventDefault();
+	// 	e.preventDefault();
 		
-		$('#successAlert').slideDown();
+	// 	$('#successAlert').slideDown();
 		
+	// });
+	
+	// $('a.pop').click(function(e) {
+	// 	e.preventDefault();
+	// });
+	
+	// $('a.pop').popover();
+	
+	// $('[rel="tooltip"]').tooltip();
+
+	$('button').on('click', function(){
+  		$('body').toggleClass('open');
 	});
-	
-	$('a.pop').click(function(e) {
-		e.preventDefault();
-	});
-	
-	$('a.pop').popover();
-	
-	$('[rel="tooltip"]').tooltip();
 	
 	
 });
